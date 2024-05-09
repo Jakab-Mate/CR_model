a546a871761ab05b3eab2dfb30c2295120e3a879
Jakab-Mate <mate.jakab@ecolres.hu>
2024-05-09 12:04:47 +0200

    Fixed error message

--- sample_pool.jl
+++ sample_pool.jl
@@ -2,11 +2,13 @@ using StatsBase, Random
 
 include("structs.jl")
 
-function sample_pool(rng, p, n_resources::Int64; n_sampled::Int64=10)
+function sample_pool(rng, p, n_resources::Int64, n_species::Int64=10, n_invaders::Int64=10)
+
+    n_sampled = n_species + n_invaders
 
     size_of_pool = size(p.pool, 3)
     if size_of_pool < n_sampled
-        throw(DomainError(ch, "\n Number sampled ($n_sampled) should be smaller than the pool ($size_of_pool) \n"))
+        throw(DomainError(n_sampled, "\n Number sampled ($n_sampled) should be smaller than the pool ($size_of_pool) \n"))
     end
 
     species_indices = sample(rng, 1:size_of_pool, n_sampled, replace=false)
@@ -17,7 +19,7 @@ function sample_pool(rng, p, n_resources::Int64; n_sampled::Int64=10)
     species_n_reactions = p.n_reactions[species_indices]
     species_n_splits = p.n_splits[species_indices]
 
-    species_initial_abundances = rand!(rng, zeros(n_sampled))
+    species_initial_abundances = vcat(rand!(rng, zeros(n_species)), zeros(n_invaders))
     resource_initial_abundances = rand!(rng, zeros(n_resources))
 
     return sample_struct(n_sampled, species_C_matrices, species_family_ids, species_m, species_n_reactions, species_n_splits, 
