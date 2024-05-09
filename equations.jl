fe0d4cded56bcfaaa0b1b24b5d92959f238e7348
Jakab-Mate <mate.jakab@ecolres.hu>
2024-05-09 15:30:29 +0200

    Changed to only compute for present species

--- equations.jl
+++ equations.jl
@@ -1,12 +1,11 @@
 using LinearAlgebra
 
 function equations(u, p, t)
-    n_species, n_resources, present_species, C, D, W_ba, n_reactions, n_splits, m, phi, eta, tau, alpha = p
     #du[n_species+1:end] = (alpha .- u[n_species+1:end]) ./ tau
     #du[1:n_species] = zeros(Float64, n_species)
-    species = zeros(Float64, n_species)
-    consumption = zeros(Float64, n_resources)
-    production = zeros(Float64, n_resources)
+    species = zeros(Float64, p.n_species)
+    consumption = zeros(Float64, p.n_resources)
+    production = zeros(Float64, p.n_resources)
 
     # print("\n Shape u:", size(u), "Type:", eltype(u))
     # print("\n Shape C:", size(C), "Type:", eltype(C))
@@ -20,32 +19,23 @@ function equations(u, p, t)
     # print("\n D*res", print(D .* u[n_species+1:end]), "Type:", eltype(D .* u[n_species+1:end]))
     # print("\n t: ", t)
 
-    for i in 1:n_species
-        if i == 4
-            if t > 100
-                # print("\n W_ba=", W_ba, "\n")
-                # print("\n D= ", D, "\n")
-                # print("C*W_ba= ", C[:,:,i] .* W_ba, "\n")
-                # print("C*W_ba*res= ", C[:,:,i] .* W_ba .* u[n_species+1:end], "\n")
-                # print("sum= ", sum(C[:,:,i] .* W_ba .* u[n_species+1:end]), "\n")
-            end
-        end
-        species[i] = (sum(C[:,:,i] .* W_ba .* u[n_species+1:end]') - (m[i] + phi * n_reactions[i] + eta * n_splits[i])) * u[i]
+    for i in p.present_species
+        species[i] = (sum(p.C[:,:,i] .* p.W_ba .* u[p.n_species+1:end]') - (p.m[i] + p.phi * p.n_reactions[i] + p.eta * p.n_splits[i])) * u[i]
         #print(size(C[:,:,i]))
         #print(size(C[:,:,i] .* W_ba))
         #print(size(C[:,:,i] .* W_ba .* u[n_species+1:end]))
         #print(typeof(species[i]))
         #print(size(sum(C[:,:,i] .* W_ba .* u[n_species+1:end])))
 
-        consumption += dropdims(sum(C[:, :, i] .* u[i] .* u[n_species+1:end]', dims=1)', dims=2)
-        production += dropdims(sum(D .* C[:, :, i] .* u[i] .* u[n_species+1:end]', dims=2), dims=2)
+        consumption += dropdims(sum(p.C[:, :, i] .* u[i] .* u[p.n_species+1:end]', dims=1)', dims=2)
+        production += dropdims(sum(p.D .* p.C[:, :, i] .* u[i] .* u[p.n_species+1:end]', dims=2), dims=2)
 
         #print("\n Shape prod", size(production), "Type:", eltype(production))
         #print("\n Shape con", size(consumption), "Type:", eltype(consumption))
 
     end
 
-    resources = (alpha .- u[n_species+1:end]) ./ tau - consumption + production
+    resources = (p.alpha .- u[p.n_species+1:end]) ./ p.tau - consumption + production
     
     #print("\n Shape res", size(resources), "Type:", eltype(resources))
     #print("\n Shape spec", size(species), "Type:", eltype(species))
